1ca1cb7a4f665264c4255926774867f1055c5a46
Artemisee77 <a123z4i6z@gmail.com>
2025-12-04 13:06:36 +0700

    commit end(insyaallah)

--- Vending.py
+++ Vending.py
@@ -6,19 +6,33 @@ folder = os.path.dirname(__file__)
 
 root = tk.Tk()
 root.title("Vending Machine Kel 1")
-root.geometry("400x800")
-tempat_uang = tk.Frame(root, bg="white")
-tempat_uang.pack(pady=5, fill="x")
-tk.Label(tempat_uang, text="Masukkan Uang anda:", bg="#f0f0f0").pack(side="left", padx=10)
+root.geometry("1000x800")
 
-entry_uang = tk.Entry(tempat_uang, width=8)
+wadah_utama = tk.Frame(root)
+wadah_utama.pack(fill="both", expand=True)
+
+frame_kiri = tk.Frame(wadah_utama, bg="white")
+frame_kiri.pack(side="left", fill="both", expand=True, padx=10, pady=10)
+
+frame_kanan = tk.Frame(wadah_utama, bg="lightgray", width=350)
+frame_kanan.pack(side="right", fill="y", padx=10, pady=10)
+frame_kanan.pack_propagate(False)
+
+tempat_uang = tk.Frame(frame_kanan, bg="lightgray",)
+tempat_uang.pack(pady=20, fill="x")
+tk.Label(tempat_uang, text="Masukkan Uang anda:", bg="#FEFEFE").pack(side="left", padx=10)
+
+frame_input = tk.Frame(tempat_uang, bg="lightgray")
+frame_input.pack(pady=5)
+
+entry_uang = tk.Entry(frame_input, width=10)
 entry_uang.pack(side="left", padx=5)
 
-btn_uang = tk.Button(tempat_uang, text="Masukkan", bg="lightblue", command=lambda: tambah_uang())
-btn_uang.pack(side="left", padx=5)
+btn_uang = tk.Button(frame_input, text="Masukkan", bg="lightblue", command=lambda: tambah_uang())
+btn_uang.pack(side="left")
 
 lbl_saldo = tk.Label(tempat_uang, text="Saldo: Rp 0", font=("arial", 12, "bold"),fg="white", bg="blue")
-lbl_saldo.pack(side="right", padx=10)
+lbl_saldo.pack(pady=10)
 
 
 def layout_gambar(nama_file):
@@ -91,18 +105,18 @@ def clear_btn():
     Layar.delete(0, tk.END)
 
 def show_struk(produk, kembalian):
-    jendela_struk = tk.Toplevel(root)
-    jendela_struk.title("Struk Pembelian")
-    jendela_struk.geometry("300x450")
+    struk_window = tk.Toplevel(root)
+    struk_window.title("Struk Pembelian")
+    struk_window.geometry("300x450")
     nama_file = produk["file"]
     gambar = simpan_gambar.get(nama_file)
 
     if gambar:
-        tk.Label(jendela_struk, image=gambar).pack(pady=20)
+        tk.Label(struk_window, image=gambar).pack(pady=20)
 
     info_transaksi = f"Barang yang dibeli:\n{produk['nama']}\n\nKembalian Anda:\nRp {kembalian:,}"  
-    tk.Label(jendela_struk, text=info_transaksi, font=("arial", 12, "bold")).pack(pady=10)
-    tk.Button(jendela_struk, text="Ambil", bg="green", fg="white", command=jendela_struk.destroy).pack(pady=20)
+    tk.Label(struk_window, text=info_transaksi, font=("arial", 12, "bold")).pack(pady=10)
+    tk.Button(struk_window, text="Ambil", bg="green", fg="white", command=struk_window.destroy).pack(pady=20)
 
 def bayar():
     global saldo
@@ -140,16 +154,16 @@ def bayar():
 
 simpan_gambar = {}
 
-etalase = tk.Frame(root, bg="white")
-etalase.pack(pady="2")
+etalase = tk.Frame(frame_kiri, bg="white")
+etalase.pack(fill="both", expand=True)
 
 def tampilkan_etalase(nomor_halaman):
 
     for widget in etalase.winfo_children():
         widget.destroy()
 
-    indeks_awal = nomor_halaman * 9
-    indeks_akhir = indeks_awal + 9
+    indeks_awal = nomor_halaman * 25
+    indeks_akhir = indeks_awal + 25
 
     halaman_tampil = daftar_produk[indeks_awal : indeks_akhir]
 
@@ -167,26 +181,31 @@ def tampilkan_etalase(nomor_halaman):
         
         gambar_jadi = simpan_gambar[nama_file_gambar]
 
-        kotak_produk = tk.Frame(etalase, bd=2, relief="ridge", bg="white")
-        kotak_produk.grid(row=baris, column=kolom, padx=2, pady=2)
+        kotak_produk = tk.Frame(etalase, bd=2, relief="groove", bg="white", width=105, height=140)
+        kotak_produk.grid(row=baris, column=kolom, padx=5, pady=5)
+        kotak_produk.pack_propagate(False)
+
+        nomor = (baris * 5) + kolom + 1
+        lbl_no = tk.Label(kotak_produk, text=str(nomor), font=("arial, 7"))
+        lbl_no.place(relx=1.0, x=0, y=0, anchor="ne")
         
         lbl_img = tk.Label(kotak_produk, image=gambar_jadi, bg="white")
-        lbl_img.pack(padx=1, pady=1)
+        lbl_img.pack(pady=(10, 0))
         
         lbl_nama = tk.Label(kotak_produk, text=nama_produk_asli, font=("arial", 7, "bold"), bg="white", wraplength=80)
-        lbl_nama.pack(padx=1, pady=0)
+        lbl_nama.pack(padx=0, pady=0)
         
-        text_harga = f"Rp {harga_produk_asli:,}" 
+        text_harga = f"Rp {harga_produk_asli:,}"
         lbl_harga = tk.Label(kotak_produk, text=text_harga, font=("arial", 7), fg="green", bg="white")
-        lbl_harga.pack(padx=5, pady=2)
+        lbl_harga.pack(pady=0, padx=0)
 
         kolom += 1
-        if kolom > 2:
+        if kolom > 4:
             kolom = 0
             baris += 1
 
-Kontrol = tk.Frame(root, bg="lightgray")
-Kontrol.pack(padx=10, pady=5)
+Kontrol = tk.Frame(frame_kanan, bg="lightgray")
+Kontrol.pack(pady=20)
 
 Layar = tk.Entry(Kontrol, width=20, font=("arial", 20), justify="right")
 Layar.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
